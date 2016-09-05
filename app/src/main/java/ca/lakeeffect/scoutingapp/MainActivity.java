36ad9739e958aab45281481947acf56c5b16f476
thebestusernameever <ajay@ajayinkingston.com>
2016-09-04 22:13:15 -0400

    Added clicking stuff

--- app/src/main/java/ca/lakeeffect/scoutingapp/MainActivity.java
+++ app/src/main/java/ca/lakeeffect/scoutingapp/MainActivity.java
@@ -1,13 +1,20 @@
 package ca.lakeeffect.scoutingapp;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +22,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     List<Counter> counters = new ArrayList<>();
     List<CheckBox> checkboxes = new ArrayList<>();
-    List<RadioButton> radioButtons = new ArrayList<>();
+    List<RadioGroup> radiogroups = new ArrayList<>();
     List<Button> buttons = new ArrayList<>();
-    List<SeekBar> seekbar = new ArrayList<>();
+    List<SeekBar> seekbars = new ArrayList<>();
 
     Button submit;
+
+    int robotNum = 0000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +44,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         submit = (Button) findViewById(R.id.submitButton);
 
         //add onClickListeners
+
+        submit.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
+        if(v == submit){
+            saveData();
+        }
+    }
+
+    public void saveData(){
+        File sdCard = Environment.getExternalStorageDirectory();
+        File dir = new File (sdCard.getAbsolutePath() + "/ScoutingData/");
+
+        File file = new File(dir, robotNum + ".txt");
+
+        FileOutputStream f = null;
+        try {
+            f = new FileOutputStream(file);
+
+            OutputStreamWriter myOutWriter = new OutputStreamWriter(f);
+
+            for(Counter counter: counters){
+                myOutWriter.append("counter " + getResources().getResourceEntryName(counter.getId()) + " " + counter.count + "\n");
+            }
+
+            for(CheckBox checkbox: checkboxes){
+                myOutWriter.append("checkbox " + getResources().getResourceEntryName(checkbox.getId()) + " " + checkbox.isChecked() + "\n");
+            }
+
+            for(RadioGroup radiogroup: radiogroups){
+                myOutWriter.append("radiogroup " + getResources().getResourceEntryName(radiogroup.getId()) + " " + radiogroup.indexOfChild(findViewById(radiogroup.getCheckedRadioButtonId())) + "\n");
+            }
+
+//            TODO: Write button data, might not be needed
+//            for(Button button: buttons){
+//                myOutWriter.append("button " + getResources().getResourceEntryName(button.getId()) + " " + counter.count + "\n");
+//            }
+
+            for(SeekBar seekbar: seekbars){
+                myOutWriter.append("seekbar " + getResources().getResourceEntryName(seekbar.getId()) + " " + seekbar.getProgress() + "\n");
+            }
+
+            myOutWriter.close();
+
+            f.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
