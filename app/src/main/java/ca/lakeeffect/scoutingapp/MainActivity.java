37f62648370c27f2a0585258c60301e49fba9816
thebestusernameever <ajay@ajayinkingston.com>
2016-09-20 17:35:45 -0400

    Initial Commit

--- app/src/main/java/ca/lakeeffect/scoutingapp/MainActivity.java
+++ app/src/main/java/ca/lakeeffect/scoutingapp/MainActivity.java
@@ -145,8 +145,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case 1: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
